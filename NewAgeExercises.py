ae8f1a21c9590fcd8bfff34b18bd9e9007ecfe0a
samuelhermon <145871063+samuelhermon@users.noreply.github.com>
2024-05-04 16:56:01 -0400

    Add files via upload
    How old will you be in 100 years?

---
+++ NewAgeExercises.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+age = int(input(name + ", how old are you?"))  # Convert age input to integer
+NewAge = 2024 - age + 100  # Calculate the year when the person will be 100 years old
+print(name, "will be 100 years old in the year", NewAge)
+
+
+
